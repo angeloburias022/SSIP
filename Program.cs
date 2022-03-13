4248d238065ba192f47afee063d7759ffecd5fb0
angelo burias <angeloburias8@gmail.com>
2022-03-13 14:54:55 +0800

    sample

--- Program.cs
+++ Program.cs
@@ -15,17 +15,17 @@ namespace SSIP
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard());
-
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Dashboard());
 
-            //Login frmMain = new Login();
-            //frmMain.Show();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-            //Application.Run();
+            Login frmMain = new Login();
+            frmMain.Show();
+
+            Application.Run();
         }
     }
 }
