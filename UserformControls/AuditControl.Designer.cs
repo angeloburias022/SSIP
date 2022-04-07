95ca58eeedf5476b262041f285be478fd00c3213
angelo burias <angeloburias8@gmail.com>
2022-04-07 13:28:12 +0800

    extra layer of security for audit

--- UserformControls/AuditControl.Designer.cs
+++ UserformControls/AuditControl.Designer.cs
@@ -29,11 +29,19 @@ namespace SSIP.UserformControls
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             this.auditGrid = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.access_panel = new System.Windows.Forms.Panel();
+            this.confirmAccessPanel = new System.Windows.Forms.Panel();
+            this.lbl_confirmAccess = new System.Windows.Forms.Label();
+            this.tb_unameAccess = new Guna.UI2.WinForms.Guna2TextBox();
+            this.tb_pass = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btn_access = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.auditGrid)).BeginInit();
+            this.access_panel.SuspendLayout();
+            this.confirmAccessPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // auditGrid
@@ -43,29 +51,29 @@ namespace SSIP.UserformControls
             this.auditGrid.AllowUserToOrderColumns = true;
             this.auditGrid.AllowUserToResizeColumns = false;
             this.auditGrid.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            this.auditGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            this.auditGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
             this.auditGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.auditGrid.BackgroundColor = System.Drawing.Color.White;
             this.auditGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.auditGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
             this.auditGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.5F);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.auditGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.auditGrid.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.auditGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.auditGrid.DefaultCellStyle = dataGridViewCellStyle9;
             this.auditGrid.EnableHeadersVisualStyles = false;
             this.auditGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.auditGrid.Location = new System.Drawing.Point(39, 95);
@@ -97,16 +105,132 @@ namespace SSIP.UserformControls
             this.auditGrid.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.auditGrid.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             // 
+            // access_panel
+            // 
+            this.access_panel.BackColor = System.Drawing.Color.Transparent;
+            this.access_panel.Controls.Add(this.confirmAccessPanel);
+            this.access_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.access_panel.Location = new System.Drawing.Point(0, 0);
+            this.access_panel.Name = "access_panel";
+            this.access_panel.Size = new System.Drawing.Size(1055, 518);
+            this.access_panel.TabIndex = 1;
+            // 
+            // confirmAccessPanel
+            // 
+            this.confirmAccessPanel.BackColor = System.Drawing.Color.Transparent;
+            this.confirmAccessPanel.Controls.Add(this.btn_access);
+            this.confirmAccessPanel.Controls.Add(this.lbl_confirmAccess);
+            this.confirmAccessPanel.Controls.Add(this.tb_unameAccess);
+            this.confirmAccessPanel.Controls.Add(this.tb_pass);
+            this.confirmAccessPanel.Location = new System.Drawing.Point(295, 140);
+            this.confirmAccessPanel.Name = "confirmAccessPanel";
+            this.confirmAccessPanel.Size = new System.Drawing.Size(448, 241);
+            this.confirmAccessPanel.TabIndex = 174;
+            // 
+            // lbl_confirmAccess
+            // 
+            this.lbl_confirmAccess.AutoSize = true;
+            this.lbl_confirmAccess.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_confirmAccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_confirmAccess.ForeColor = System.Drawing.Color.Honeydew;
+            this.lbl_confirmAccess.Location = new System.Drawing.Point(65, 40);
+            this.lbl_confirmAccess.Name = "lbl_confirmAccess";
+            this.lbl_confirmAccess.Size = new System.Drawing.Size(331, 20);
+            this.lbl_confirmAccess.TabIndex = 122;
+            this.lbl_confirmAccess.Text = "HIGHER AUTHORIZED PERSON ONLY";
+            // 
+            // tb_unameAccess
+            // 
+            this.tb_unameAccess.BackColor = System.Drawing.Color.Transparent;
+            this.tb_unameAccess.BorderRadius = 4;
+            this.tb_unameAccess.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_unameAccess.DefaultText = "";
+            this.tb_unameAccess.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.tb_unameAccess.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.tb_unameAccess.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_unameAccess.DisabledState.Parent = this.tb_unameAccess;
+            this.tb_unameAccess.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_unameAccess.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.tb_unameAccess.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_unameAccess.FocusedState.Parent = this.tb_unameAccess;
+            this.tb_unameAccess.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tb_unameAccess.ForeColor = System.Drawing.Color.Black;
+            this.tb_unameAccess.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_unameAccess.HoverState.Parent = this.tb_unameAccess;
+            this.tb_unameAccess.Location = new System.Drawing.Point(154, 89);
+            this.tb_unameAccess.Name = "tb_unameAccess";
+            this.tb_unameAccess.PasswordChar = '\0';
+            this.tb_unameAccess.PlaceholderText = "Username";
+            this.tb_unameAccess.SelectedText = "";
+            this.tb_unameAccess.ShadowDecoration.Parent = this.tb_unameAccess;
+            this.tb_unameAccess.Size = new System.Drawing.Size(165, 36);
+            this.tb_unameAccess.TabIndex = 124;
+            this.tb_unameAccess.TextChanged += new System.EventHandler(this.tb_unameAccess_TextChanged);
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.BackColor = System.Drawing.Color.Transparent;
+            this.tb_pass.BorderRadius = 4;
+            this.tb_pass.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_pass.DefaultText = "";
+            this.tb_pass.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.tb_pass.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.tb_pass.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_pass.DisabledState.Parent = this.tb_pass;
+            this.tb_pass.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tb_pass.Enabled = false;
+            this.tb_pass.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.tb_pass.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_pass.FocusedState.Parent = this.tb_pass;
+            this.tb_pass.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tb_pass.ForeColor = System.Drawing.Color.Black;
+            this.tb_pass.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tb_pass.HoverState.Parent = this.tb_pass;
+            this.tb_pass.Location = new System.Drawing.Point(154, 134);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.PasswordChar = '*';
+            this.tb_pass.PlaceholderText = "Password";
+            this.tb_pass.SelectedText = "";
+            this.tb_pass.ShadowDecoration.Parent = this.tb_pass;
+            this.tb_pass.Size = new System.Drawing.Size(165, 36);
+            this.tb_pass.TabIndex = 123;
+            // 
+            // btn_access
+            // 
+            this.btn_access.BackColor = System.Drawing.Color.Transparent;
+            this.btn_access.BorderRadius = 4;
+            this.btn_access.CheckedState.Parent = this.btn_access;
+            this.btn_access.CustomImages.Parent = this.btn_access;
+            this.btn_access.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_access.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_access.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_access.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_access.DisabledState.Parent = this.btn_access;
+            this.btn_access.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.btn_access.ForeColor = System.Drawing.Color.White;
+            this.btn_access.HoverState.Parent = this.btn_access;
+            this.btn_access.Location = new System.Drawing.Point(183, 186);
+            this.btn_access.Name = "btn_access";
+            this.btn_access.ShadowDecoration.Parent = this.btn_access;
+            this.btn_access.Size = new System.Drawing.Size(111, 40);
+            this.btn_access.TabIndex = 171;
+            this.btn_access.Text = "ACCESS";
+            this.btn_access.Click += new System.EventHandler(this.btn_access_Click);
+            // 
             // AuditControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SSIP.Properties.Resources.bg;
+            this.Controls.Add(this.access_panel);
             this.Controls.Add(this.auditGrid);
             this.Name = "AuditControl";
             this.Size = new System.Drawing.Size(1055, 518);
             this.Load += new System.EventHandler(this.AuditControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.auditGrid)).EndInit();
+            this.access_panel.ResumeLayout(false);
+            this.confirmAccessPanel.ResumeLayout(false);
+            this.confirmAccessPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,5 +238,11 @@ namespace SSIP.UserformControls
         #endregion
 
         private Guna.UI2.WinForms.Guna2DataGridView auditGrid;
+        private System.Windows.Forms.Panel access_panel;
+        private System.Windows.Forms.Panel confirmAccessPanel;
+        private System.Windows.Forms.Label lbl_confirmAccess;
+        private Guna.UI2.WinForms.Guna2TextBox tb_unameAccess;
+        private Guna.UI2.WinForms.Guna2TextBox tb_pass;
+        private Guna.UI2.WinForms.Guna2Button btn_access;
     }
 }
--- UserformControls/AuditControl.cs
+++ UserformControls/AuditControl.cs
@@ -1,4 +1,6 @@
 ﻿using SSIP.Controllers;
+using SSIP.Helper;
+using SSIP.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -33,6 +35,37 @@ namespace SSIP.UserformControls
         private void AuditControl_Load(object sender, EventArgs e)
         {
             UpdateGrid();
+        }
+
+        private void tb_unameAccess_TextChanged(object sender, EventArgs e)
+        {
+            if (tb_unameAccess.Text.Length != 0)
+            {
+                tb_pass.Enabled = true;
+            }
+            else
+            {
+                tb_pass.Enabled = false;
+            }
+        }
+
+        private void btn_access_Click(object sender, EventArgs e)
+        {
+            var creds = new User
+            {
+                Username = tb_unameAccess.Text,
+                Password = tb_pass.Text
+            };
+            
+
+            if (Access.HighAuthorityAccess(creds))
+            {
+                access_panel.Visible = false;
+            }
+            else
+            {
+                access_panel.Visible = false;
+            }
         }
     }
 }
