d16734202f4241c0d8e31d24e7484416270659f7
angelo burias <angeloburias8@gmail.com>
2022-04-07 13:27:56 +0800

    added new helper access security

---
+++ Helper/Access.cs
@@ -0,0 +1,45 @@
+﻿using SSIP.Controllers;
+using SSIP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSIP.Helper
+{
+    public static class Access
+    {
+        public static bool HighAuthorityAccess(User user)
+        {
+            var tools = new AccessController();
+
+            var result = tools.ConfirmAuthority(user);
+
+            if (result != false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool AuthorizeAccess(User user)
+        {
+            var tools = new AccessController();
+
+            var result = tools.ConfirmAccess(user);
+
+            if (result != false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
